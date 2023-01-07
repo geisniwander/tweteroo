5b8e3b8ae783cb58120b31ba82564d79c32142ee
geisniwander <geisniwander@gmail.com>
2023-01-07 12:40:39 -0300

    Endpoints now correctly respond to requests with status codes and messages. Endpoint getTweets adds avatar to each tweet and only sends the last 10

--- src/app.mjs
+++ src/app.mjs
@@ -1,6 +1,5 @@
-import express from 'express';
-import cors from 'cors';
-
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
@@ -10,20 +9,27 @@ const tweets = [];
 const users = [];
 
 app.post("/sign-up", (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.send(user);
-})
+  const user = req.body;
+  users.push(user);
+  res.send("OK");
+});
 
 app.post("/tweets", (req, res) => {
-    const tweet = req.body;
-    tweets.push(tweet);
-    res.send(tweet);
-})
+  const content = req.body;
+  const name = req.body.username;
+  const tweet = req.body.tweet;
+  const isRegistered = users.find((user) => user.username === name);
+  if (!isRegistered) return res.status(401).send("UNAUTHORIZED");
+  tweets.push(content);
+  res.send(tweet);
+});
 
 app.get("/tweets", (req, res) => {
-    res.send(tweets);
-})
+  const last10 = tweets.reverse().slice(0, 10);
+  last10.map((l) => {
+    l.avatar = users.find((user) => user.username === l.username).avatar;
+  });
+  res.send(last10);
+});
 
-
-app.listen(5020);
+app.listen(5000);
