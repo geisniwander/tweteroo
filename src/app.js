58bc9122c31d8fedd0aabf62b823a015df6cc76e
geisniwander <geisniwander@gmail.com>
2023-01-07 15:45:43 -0300

    Returning tweets with pagination, via query string

--- src/app.js
+++ src/app.js
@@ -5,8 +5,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const tweets = [];
-const users = [];
+const tweets = [
+  { username: "teste", tweet: "1" },
+  { username: "gps", tweet: "2" },
+  { username: "gps", tweet: "3" },
+  { username: "gps", tweet: "4" },
+  { username: "gps", tweet: "5" },
+  { username: "teste", tweet: "6" },
+  { username: "gps", tweet: "7" },
+  { username: "gps", tweet: "8" },
+  { username: "teste", tweet: "9" },
+  { username: "gps", tweet: "10" },
+  { username: "gps", tweet: "11" },
+  { username: "gps", tweet: "12" },
+  { username: "teste", tweet: "13" },
+  { username: "teste", tweet: "14" },
+  { username: "gps", tweet: "15" },
+  { username: "gps", tweet: "16" },
+  { username: "gps", tweet: "17" },
+  { username: "gps", tweet: "18" },
+  { username: "gps", tweet: "19" },
+  { username: "gps", tweet: "20" },
+];
+const users = [
+  { username: "gps", avatar: "https://www.google.com" },
+  { username: "teste", avatar: "https://www.google.com" },
+];
 
 app.post("/sign-up", (req, res) => {
   const userObj = req.body;
@@ -21,10 +45,8 @@ app.post("/sign-up", (req, res) => {
   } catch (err) {
     isURL = false;
   }
-  if (!username || typeof username !== "string") 
-    return res.sendStatus(400);
-  if (!avatar || !isURL) 
-    return res.sendStatus(400);
+  if (!username || typeof username !== "string") return res.sendStatus(400);
+  if (!avatar || !isURL) return res.sendStatus(400);
   users.push(userObj);
   res.status(201).send("OK");
 });
@@ -36,27 +58,40 @@ app.post("/tweets", (req, res) => {
   const isRegistered = users.find((user) => user.username === username);
   if (username === "" || tweet === "")
     return res.status(400).send("Todos os campos são obrigatórios!");
-  if (!tweet || typeof tweet !== "string") 
-    return res.sendStatus(400);
-  if (!isRegistered) 
-    return res.status(401).send("UNAUTHORIZED");
+  if (!tweet || typeof tweet !== "string") return res.sendStatus(400);
+  if (!isRegistered) return res.status(401).send("UNAUTHORIZED");
   tweets.push(tweetObj);
   res.status(201).send(tweetObj);
 });
 
 app.get("/tweets", (req, res) => {
-  const {USERNAME} = req.query;
-  if(!USERNAME){
+  const { USERNAME, page } = req.query;
+  if (page && page !== "") {
+    const numPage = Number(page);
+    if (!numPage || numPage <= 0)
+      return res.status(400).send("Informe uma página válida!");
+    if (numPage) {
+      const arrayTweets = [...tweets];
+      const initialTweet = numPage * 10 - 10;
+      const finalTweet = initialTweet + 10;
+      const tweetPage = arrayTweets.reverse().slice(initialTweet, finalTweet);
+      tweetPage.map((l) => {
+        l.avatar = users.find((user) => user.username === l.username).avatar;
+      });
+      if (tweetPage.length === 0)
+        return res.status(404).send("Informe uma página válida!");
+      return res.send(tweetPage);
+    }
+  } else if (!USERNAME && !page) {
     const last10 = tweets.reverse().slice(0, 10);
     last10.map((l) => {
       l.avatar = users.find((user) => user.username === l.username).avatar;
     });
-    res.send(last10);
-  }else{
-    const userTweets = tweets.filter(t => t.username===USERNAME);
-    res.send(userTweets)
+    return res.status(200).send(last10);
+  } else {
+    const userTweets = tweets.filter((t) => t.username === USERNAME);
+    res.status(200).send(userTweets);
   }
-
 });
 
 app.listen(5000);
